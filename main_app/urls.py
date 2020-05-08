bd744a4afc26065650e949a2e26834d02d772526
Amandabuch <amandalbuch@gmail.com>
2020-05-08 15:53:19 -0700

    Create and Delete functionality working

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('add/', views.WishCreate.as_view(), name='add'),
+  path('<int:pk>/delete/', views.WishDelete.as_view(), name='wish_delete')
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic.edit import CreateView, DeleteView
 
 from django.http import HttpResponse
 from .models import Wish
@@ -11,3 +10,12 @@ def home(request):
     return render(request, 'home.html', { 'wishs': wishs })
 
 
+class WishCreate(CreateView):
+  model = Wish
+  fields = '__all__'
+  success_url = '/'
+
+
+class WishDelete(DeleteView):
+  model = Wish
+  success_url = '/'
