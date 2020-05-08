cf2c04315cbcc45f693fee43c5eeb5253d158746
Amandabuch <amandalbuch@gmail.com>
2020-05-08 15:01:13 -0700

    Able to see wish on home page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Wish(models.Model):
+    description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.description
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from .models import Wish
+
+# Define the home view
+def home(request):
+    wishs = Wish.objects.all()
+    return render(request, 'home.html', { 'wishs': wishs })
+
+
