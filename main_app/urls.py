dd296cd24cee09d3ebd7592accfa29829bbab03b
Amandabuch <amandalbuch@gmail.com>
2020-05-09 10:57:17 -0700

    When no wishs other message pops up

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # BEFORE CODE
   path('', views.home, name='home'),
+# AfterCODE
+#   path('', views.WishList.as_view(), name='wish_list'),
   path('add/', views.WishCreate.as_view(), name='add'),
   path('<int:pk>/delete/', views.WishDelete.as_view(), name='wish_delete')
 ]
 
+# path('cats/', views.CatList.as_view(), name='cats_index'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.views.generic.edit import CreateView, DeleteView
 
 from django.http import HttpResponse
 from .models import Wish
 
 # Define the home view
+# Before code
 def home(request):
     wishs = Wish.objects.all()
     return render(request, 'home.html', { 'wishs': wishs })
 
+# AfterCODE
+# class WishList(ListView):
+#     model = Wish
+#     wishs = Wish.objects.all()
+
+#     def get_queryset(self):
+#         return wishs
 
 class WishCreate(CreateView):
   model = Wish
@@ -19,3 +28,10 @@ class WishCreate(CreateView):
 class WishDelete(DeleteView):
   model = Wish
   success_url = '/'
+
+
+#   class CatList(ListView):
+#     model = Cat
+
+#     def get_queryset(self):
+#         return Cat.objects.all()
